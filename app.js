6240db1c1849ae91c9810cb3809d3217c05536ef
Eric Cavalcanti <111467080+ecavalcanti93@users.noreply.github.com>
2024-12-03 15:57:31 +0100

    Add files via upload

---
+++ app.js
@@ -0,0 +1,65 @@
+function sortear() {
+    let quantidade = parseInt(document.getElementById("quantidade").value);
+    let numMinimo = parseInt(document.getElementById("de").value);
+    let numMaximo = parseInt(document.getElementById("ate").value);
+
+    let sorteados = [];
+    let numero;
+    
+    for(let i = 0; i < quantidade; i++) {
+        numero = numeroAleatorio(numMinimo, numMaximo);
+        
+        while (sorteados.includes(numero)) {
+            numero = numeroAleatorio(numMinimo, numMaximo);
+        }
+        sorteados.push(numero);
+    }
+
+    let resultado = document.getElementById("resultado");
+    resultado.innerHTML = `<label class="texto__paragrafo">Numbers drawn: ${sorteados} </label>`
+    statusBotao();
+}
+
+function verificarCampos() {
+    let quantidade = document.getElementById("quantidade").value;
+    let numMinimo = document.getElementById("de").value;
+    let numMaximo = document.getElementById("ate").value;
+    let botao = document.getElementById("btn-sortear");
+
+    if (quantidade && numMinimo && numMaximo && quantidade > 0) {
+        botao.disabled = false; 
+    } else {
+        botao.disabled = true; 
+    }
+}
+
+// Adiciona o evento 'input' aos campos de entrada para monitorar mudanças
+document.getElementById("quantidade").addEventListener("input", verificarCampos);
+document.getElementById("de").addEventListener("input", verificarCampos);
+document.getElementById("ate").addEventListener("input", verificarCampos);
+
+
+function numeroAleatorio(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min; 
+}
+
+function statusBotao(){
+    let botao = document.getElementById("btn-reiniciar");
+
+    if (botao.classList.contains("container__botao-desabilitado")) {
+        botao.classList.remove("container__botao-desabilitado");
+        botao.classList.add("container__botao")
+    } else {
+        botao.classList.remove("container__botao");
+        botao.classList.add("container__botao-desabilitado")
+}
+}
+
+function reiniciar() {
+    document.getElementById("quantidade").value = " ";
+    document.getElementById("de").value = " ";
+    document.getElementById("ate").value = " ";
+    document.getElementById("resultado").innerHTML = '<label class="texto__paragrafo">Numbers drawn: none so far...</label>';
+    statusBotao();
+    verificarCampos();
+}
